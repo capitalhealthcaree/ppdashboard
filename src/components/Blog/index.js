f106d8a51e8a333af2369651752fd6357266e59e
capitalhealthcaree <premierpatientappointment@gmail.com>
2023-05-03 23:33:32 -0700

    remove commented code

--- src/components/Blog/index.js
+++ src/components/Blog/index.js
@@ -1,16 +1,10 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef } from "react";
 import { Button, Form, FormGroup, Label, Input, Row, Col } from "reactstrap";
 import JoditEditor from "jodit-react";
-
 import { TagsInput } from "react-tag-input-component";
-// import { EditorState, convertToRaw } from "draft-js";
 import { storage } from "../../firebase";
-// import draftToHtml from "draftjs-to-html";
-// import { Editor } from "react-draft-wysiwyg";
 import api from "../../services/api";
 import { toast } from "react-toastify";
-import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
-import { CKEditor } from "@ckeditor/ckeditor5-react";
 
 const Blog = () => {
   const editor = useRef(null);
@@ -22,7 +16,6 @@ const Blog = () => {
   const [category, setCategory] = useState("");
   const [image, setImage] = useState();
   const [seoTitleError, setSeoTitleError] = useState(false);
-  // const [editorState, setEditorState] = useState(EditorState.createEmpty());
   const [title, setTitle] = useState("");
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -76,30 +69,12 @@ const Blog = () => {
           <JoditEditor
             ref={editor}
             value={title}
-            // config={config}
             tabIndex={1} // tabIndex of textarea
-            onBlur={(newContent) => setContent(newContent)} // preferred to use only this option to update the content for performance reasons
+            // onBlur={(newContent) => setContent(newContent)} // preferred to use only this option to update the content for performance reasons
             onChange={(newContent) => {
               setTitle(newContent);
-              console.log("dr sahib", newContent);
             }}
           />
-          {/* <CKEditor
-            editor={ClassicEditor}
-            data={title}
-            onReady={(editor) => {
-              // You can store the "editor" and use when it is needed.
-              console.log("Editor is ready to use!", editor);
-            }}
-            onInit={(editor) => {
-              // This code will run when the editor is initialized
-              console.log("Editor is ready to use", editor);
-            }}
-            onChange={async (event, editor) => {
-              const data = await editor.getData();
-              setTitle(data);
-            }}
-          /> */}
         </FormGroup>
         <Row>
           <Col xs="6">
