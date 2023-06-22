4cb228757f0c29015fead07dfe8607465ed57ffb
capitalhealthcaree <premierpatientappointment@gmail.com>
2023-06-22 17:58:34 +0500

    add date format

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -28,41 +28,63 @@ const Appointment = () => {
   return data ? (
     <>
       <div className="row">
-        {data.map((data, index) => (
-          <div className="col-12" ref={divRef}>
-            <div class="card mb-3">
-              <div class="card-body">
-                <button
-                  className="btn btn-success"
-                  onClick={handleDownloadClick}
-                >
-                  Download Details
-                </button>
-                <div>
-                  <h5 class="d-inline mr-3">First Name:</h5>
-                  <p class="d-inline">{data.firstName}</p>
+        {data.map((data, index) => {
+          const date = new Date(data.createdAt);
+
+          const formattedDate = date.toLocaleDateString("en-US", {
+            day: "numeric",
+            month: "long",
+            year: "numeric",
+          });
+
+          const formattedTime = date.toLocaleTimeString("en-US", {
+            hour: "numeric",
+            minute: "numeric",
+            hour12: true,
+          });
+
+          const formattedDateTime = `${formattedDate}, ${formattedTime}`;
+          return (
+            <div className="col-12" ref={divRef} key={index}>
+              <div class="card mb-3">
+                <div class="card-body">
+                  <div>{formattedDateTime}</div>
+                  <button
+                    className="btn btn-success"
+                    onClick={handleDownloadClick}
+                  >
+                    Download Details
+                  </button>
+                  <div>
+                    <h5 class="d-inline mr-3">First Name:</h5>
+                    <p class="d-inline">{data.firstName}</p>
+                  </div>
+                  <div>
+                    <h5 class="d-inline mr-3">Last Name:</h5>
+                    <p class="d-inline">{data.lastName}</p>
+                  </div>
+                  <div>
+                    <h5 class="d-inline mr-3">Phone:</h5>
+                    <p class="d-inline">{data.phone}</p>
+                  </div>
+                  <div>
+                    <h5 class="d-inline mr-3">Email:</h5>
+                    <p class="d-inline">{data.email}</p>
+                  </div>
+                  <div>
+                    <h5 class="d-inline mr-3">Patient Type:</h5>
+                    <p class="d-inline">{data.patientType}</p>
+                  </div>
+
+                  <div>
+                    <h5 class="d-inline mr-3">Message:</h5>
+                    <p class="d-inline">{data.message}</p>
+                  </div>
                 </div>
-                <div>
-                  <h5 class="d-inline mr-3">Last Name:</h5>
-                  <p class="d-inline">{data.lastName}</p>
-                </div>
-                <div>
-                  <h5 class="d-inline mr-3">Phone:</h5>
-                  <p class="d-inline">{data.phone}</p>
-                </div>
-                <div>
-                  <h5 class="d-inline mr-3">Email:</h5>
-                  <p class="d-inline">{data.email}</p>
-                </div>
-                <h5 class="d-inline mr-3">Patient Type:</h5>
-                <p class="d-inline">{data.patientType}</p>
-                <div></div>
-                <h5 class="d-inline mr-3">Message:</h5>
-                <p class="d-inline">{data.message}</p>
               </div>
             </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
     </>
   ) : (
